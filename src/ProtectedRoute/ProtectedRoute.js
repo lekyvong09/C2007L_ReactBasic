d1da7363fff2d2dd445c67803fe999ba9506f723
lekyvong09 <lekyvong09@gmail.com>
2022-08-23 20:05:04 +0700

    check expire time when load token

--- src/ProtectedRoute/ProtectedRoute.js
+++ src/ProtectedRoute/ProtectedRoute.js
@@ -5,7 +5,7 @@ import AuthContext from "../store/auth-context";
 function ProtectedRoute({children}) {
     const location = useLocation();
     const authContext = useContext(AuthContext);
-    console.log('is login? ', authContext.storeIsLoggedIn);
+    // console.log('is login? ', authContext.storeIsLoggedIn);
     if (!authContext.storeIsLoggedIn) {
         return <Navigate to="/login" replace state={{from: location}} />;
     }
--- src/store/AuthProvider.js
+++ src/store/AuthProvider.js
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import AuthContext from "./auth-context";
 import jwt_decode from "jwt-decode";
+
+let logoutTimer;
+
+const validateAndGetTokenFromLocalStorage = () => {
+  if (localStorage.getItem('token')) {
+    const storedToken = localStorage.getItem('token');
+    const decodedToken = jwt_decode(storedToken);
+    if ((decodedToken.exp * 1000 - (new Date().getTime())) <=0) {
+      localStorage.removeItem('token');
+      return null;
+    }
+    return storedToken;
+  }
+}
 
 function AuthProvider(props) {
     const navigate = useNavigate();
     const location = useLocation();
 
-    const initialToken = localStorage.getItem('token');
+    const initialToken = validateAndGetTokenFromLocalStorage();
     const [token, setToken] = useState(initialToken);
-    console.log('token', token);
+    // console.log('token', token);
     const isLoggedIn = token ? true : false;
 
     const loginHandler = async (username, password) => {
@@ -32,7 +46,7 @@ function AuthProvider(props) {
 
             var decodedToken = jwt_decode(data.token);
             const remainingTime = decodedToken.exp * 1000 - (new Date().getTime());
-            setTimeout(logoutHandler, remainingTime);
+            logoutTimer = setTimeout(logoutHandler, remainingTime);
             const origin = location.state?.from?.pathname || '/shop';
             navigate(origin);
           } else {
@@ -43,11 +57,23 @@ function AuthProvider(props) {
         }
       }
     
-      const logoutHandler = () => {
+      const logoutHandler = useCallback(() => {
         setToken(null);
+        if (logoutTimer) {
+          clearTimeout(logoutTimer);
+        }
         localStorage.removeItem('token');
         navigate('/');
-      }
+      }, [navigate]);
+
+      useEffect(() => {
+        if (initialToken) {
+          var decodedToken = jwt_decode(initialToken);
+          const remainTime = decodedToken.exp * 1000 - (new Date().getTime());
+          logoutTimer = setTimeout(logoutHandler, remainTime);
+          // console.log('remaining time', remainTime);
+        }
+      }, [initialToken, logoutHandler]);
 
     return (
         <AuthContext.Provider value={{
